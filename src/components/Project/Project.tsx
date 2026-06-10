7ce3c07fe1090463147c1cc24d387a6c83cd7fd0
Realsingh123 <160656036+Realsingh123@users.noreply.github.com>
2026-06-10 13:06:34 +0530

    updated

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -83,11 +83,11 @@ export function Project() {
               <li>JS</li>
 
               
-              {/* <li>Gsap</li> */}
+              <li>Gsap</li>
             </ul>
           </footer>
         </div>
-
+{/* 
         <div className="project">
           <header>
             <svg
@@ -106,9 +106,9 @@ export function Project() {
               <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
             </svg>
             <div className="project-links">
-              {/* <a href="#" target="_blank" rel="noreferrer"> */}
+              <a href="#" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="Visit site" />
-                {/* </a> */}
+                </a>
           
               <a href="https://ashuswan3.wixsite.com/my-site-7" target="_blank" rel="noreferrer">
                 <img src={externalLink} alt="Visit site" />
@@ -121,9 +121,53 @@ export function Project() {
               Crafted a customized educational website on Wix technology. Made the required customized eye-catching components rather than predefined ones. Developed interactive maps, event calendars, and a mobile-friendly design, making your international academic journey at your fingertips.
             </p>
           </div>
+          
+
+
+
+
           <footer>
             <ul className="tech-list">
               <li>Wix</li>
+            </ul>
+          </footer>
+        </div> */}
+
+        <div className="project">
+          <header>
+            <svg
+              width="50"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#23ce6b"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <title>Folder</title>
+              <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+            </svg>
+            <div className="project-links">
+              <a href="#" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="Visit site" />
+              </a>
+              <a href="https://ashuswan3.wixsite.com/my-site-7" target="_blank" rel="noreferrer">
+                <img src={externalLink} alt="Visit site" />
+              </a>
+            </div>
+          </header>
+          <div className="body">
+            <h3>Educational website</h3>
+            <p>
+              Crafted a customized educational website on Wix technology. Made the required customized eye-catching components rather than predefined ones. Developed interactive maps, event calendars, and a mobile-friendly design, making your international academic journey at your fingertips.
+              
+            </p>
+          </div>
+          <footer>
+            <ul className="tech-list">
+              <li>wix</li>
             </ul>
           </footer>
         </div>
